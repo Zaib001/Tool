41591cba968a9e20ee6ba8c5a8d4ec410795a586
JahanZaib Malik <116706071+Zaib001@users.noreply.github.com>
2023-12-10 08:41:51 -0800

    Update index.js
    third commit

--- index.js
+++ index.js
@@ -7,13 +7,20 @@ const erroeHandling = require("./midleware/errorHandling")
 const cookieParser = require('cookie-parser');
 const app = express();
 
-
 app.use(cookieParser());
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    optionsSuccessStatus: 200,
+    credentials: true,
+  })
+);
+
+// Handle preflight requests
+app.options("*", cors());
 
 app.use(express.json({ limit: "50mb" }));
-
 app.use(router);
 
 const port = process.env.PORT || 8000;
