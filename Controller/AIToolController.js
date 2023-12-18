571d634dc05f9f243ac17fd9506c3914e6d86485
JahanZaib Malik <116706071+Zaib001@users.noreply.github.com>
2023-12-17 21:27:29 -0800

    Update AIToolController.js
    update apis

--- Controller/AIToolController.js
+++ Controller/AIToolController.js
@@ -1,93 +1,36 @@
-const AITool = require("../models/AITools");
-const AIToolForm = require('../models/formdata')
+const AiTool = require('../models/AITools');
 
 const AIController = {
-  async getAITool(req, res, next) {
-    try {
-      const page = parseInt(req.query.page) - 1 || 0;
-      const limit = parseInt(req.query.limit) || 100000;
-      const search = req.query.search || "";
-      let varient = req.query.varient || "All"; // Default to "all" if not provided
-      let sort = req.query.sort || "varient";
-      let genre = req.query.genre || "All";
-
-      const genreOptions = [
-        "AI Writing",
-        "AI Video",
-        "AI Image",
-        "AI Audio",
-        "SEO",
-        "AI Social Media",
-        "ChatBot",
-        "Data Search",
-        "AI Tools",
-      ];
-
-      genre === "All"
-        ? (genre = [...genreOptions])
-        : (genre = req.query.genre.split(","));
-      req.query.sort ? (sort = req.query.sort.split(",")) : (sort = [sort]);
-
-      let sortBy = {};
-      if (sort[1]) {
-        sortBy[sort[0]] = sort[1];
-      } else {
-        sortBy[sort[0]] = "asc";
-      }
-
-      const query = {
-        name: { $regex: search, $options: "i" },
-        genre: { $in: genre },
-        varient: { $in: varient },
-      };
-
-      // Filter by varient if it's not "all"
-      varient === "All"
-        ? (varient = ["free", "freemium", "paid", "Trial"]) // Fix typo: Change "Trail" to "Trial"
-        : (varient = req.query.varient.split(","));
-      req.query.sort ? (sort = req.query.sort.split(",")) : (sort = [sort]);
-
-      const varientQuery = { varient: { $in: varient } }; // Fix property name: Change "varient" to "variant"
-      Object.assign(query, varientQuery);
-    
-      const tools = await AITool.find(query)
-      .sort(sortBy)
-      .hint({ _id: 1 })
-      .allowDiskUse(true)
-      .skip(page * limit)
-      .limit(limit);
-
-      const total = await AITool.countDocuments(query);
-
-      const response = {
-        error: false,
-        total,
-        page: page + 1,
-        limit,
-        genres: genre,
-        tools,
-      };
-      res.status(200).json({response});
-    } catch (err) {
-      res.status(500).json({ error: true, message: "Internal Server Error" });
-    }
-  },
-  async addTools(req, res) {
-    try {
-      const newTool = new AITool(req.body);
-      const savedTool = await newTool.save();
-      res.status(201).json(savedTool);
-    } catch (error) {
-      res.status(500).json({ message: "Internal Server Error" });
-    }
-  },
   async addToolsdata(req, res) {
     try {
-      const newTool = new AIToolForm(req.body);
-      const savedTool = await newTool.save();
-      res.status(201).json(savedTool);
+      const { toolName, websiteUrl, g2ReviewUrl, toolDescription, yourName, yourEmail } = req.body;
+
+      // Validate input
+      if (!toolName || !websiteUrl || !toolDescription || !yourName || !yourEmail) {
+        return res.status(400).json({ error: 'All fields are required.' });
+      }
+
+      // Create a new instance of the AiTool model
+      const newAiTool = new AiTool({
+        toolName,
+        websiteUrl,
+        g2ReviewUrl,
+        toolDescription,
+        yourName,
+        yourEmail
+      });
+
+      // Save the tool to the database
+      const savedTool = await newAiTool.save();
+
+      // Send a response with the saved tool data
+      res.status(201).json({ message: 'AI tool added successfully', data: savedTool });
     } catch (error) {
-      res.status(500).json({ message: "Internal Server Error" });
+      // Log the error for debugging purposes
+      console.error(error);
+
+      // Send an error response
+      res.status(500).json({ message: 'Internal Server Error' });
     }
   },
 };
