08373c5c8917d63d25e92c720d0fb65e5f4a3287
JahanZaib Malik <116706071+Zaib001@users.noreply.github.com>
2023-12-17 21:28:15 -0800

    Update index.js
    routes update

--- Routes/index.js
+++ Routes/index.js
@@ -3,20 +3,12 @@ const router = express.Router();
 
 
 const AIController = require("../Controller/AIToolController");
-const paymentController = require("../Controller/PaymentController");
 
-
-
-
-// AITools 
-router.get('/AItools', AIController.getAITool);
-//post ai tools
-router.post('/addtools' , AIController.addTools)
 // formdata
 router.post('/addformdata' , AIController.addToolsdata)
 
 
-router.post('/create-checkout-session' , paymentController.create);
+// router.post('/create-checkout-session' , paymentController.create);
 
 
 
